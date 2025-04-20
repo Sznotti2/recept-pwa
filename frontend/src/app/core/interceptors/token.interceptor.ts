d312844c5fc7db2a4b24afbd3430fa0cdb441a64
Sznotti2 <tothbalint2k1@gmail.com>
2025-04-20 20:46:43 +0200

    first commit in auth branch

--- frontend/src/app/core/interceptors/token.interceptor.ts
+++ frontend/src/app/core/interceptors/token.interceptor.ts
@@ -4,17 +4,20 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // Feltételezzük, hogy a token a localStorage-ban van tárolva
-    const token = localStorage.getItem('token');
-    if (token) {
-      // Klónozzuk a requestet és beállítjuk az Authorization fejlécet
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-    }
-    return next.handle(request);
-  }
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+		const token = localStorage.getItem("token");
+
+		if (token) {
+			const cloned = req.clone({
+				headers: req.headers.set("Authorization",
+					"Bearer " + token)
+			});
+
+			return next.handle(cloned);
+		}
+		else {
+			return next.handle(req);
+		}
+	}
 }
